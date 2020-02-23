10218221e9e40534c77bc758065d22d9517a5c81
Emilio D Sforza <esforza@emiliodsforza.com>
2020-02-23 10:40:09 -0500

    Country Library
    Added Country Library To Github

---
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+   
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                    throw new ArgumentNullException(nameof(courseLibraryRepository));
+
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("api/authors/{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(id);
+            return new JsonResult(authorFromRepo);
+        }
+    }
+}
