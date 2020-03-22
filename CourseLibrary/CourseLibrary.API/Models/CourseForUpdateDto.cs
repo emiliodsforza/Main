f509e133d8b290765e8d16795248e1563c07a22d
Emilio D Sforza <esforza@emiliodsforza.com>
2020-03-21 20:27:21 -0400

    Validate PATCH

--- CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -9,6 +9,7 @@ namespace CourseLibrary.API.Models
 {
     public class CourseForUpdateDto : CourseForManipulationDto
     {
-
+        [Required(ErrorMessage = "You should fill out a description.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
     }
 }
